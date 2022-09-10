40a3ab3bdc7cfeaf88ccfd80aa34aec50c257aaa
NilEis <52820950+NilEis@users.noreply.github.com>
2022-09-10 16:37:56 +0200

    Added installer

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 #include "union_find.h"
 #include <unistd.h>
 #include "config.h"
-#include <math.h>
 
 #ifndef NDEBUG
 #define LOG(x, ...) printf(x, __VA_ARGS__)
@@ -16,9 +15,10 @@
 #endif
 
 #define WIDTH 1200
-#define HEIGHT 600
+#define HEIGHT 800
 
 RenderTexture2D target;
+RenderTexture2D target_2;
 
 typedef struct
 {
@@ -37,29 +37,25 @@ typedef struct entry
 
 void main(int argc, char **argv)
 {
-    uint32_t num_nodes = 75;
+    uint32_t num_edges_in_tree = 0;
+    uint32_t num_nodes = 150;
     if (argc == 2)
     {
         sscanf(argv[1], "%" SCNu32, &num_nodes);
     }
-    InitWindow(WIDTH, HEIGHT, "Kruskal's algorithm");
-    ToggleFullscreen();
-    target = LoadRenderTexture(WIDTH, HEIGHT);
     srand((unsigned)time(NULL));
     node_t *nodes = (node_t *)calloc(num_nodes, sizeof(node_t));
-    BeginTextureMode(target);
     for (int i = 0; i < num_nodes; i++)
     {
 
         nodes[i].x = 10 + (rand() % (WIDTH - 20));
         nodes[i].y = 10 + (rand() % (HEIGHT - 20));
         nodes[i].s = uf_make_set(&nodes[i]);
-        DrawCircle(nodes[i].x, nodes[i].y, 4, RAYWHITE);
     }
-    EndTextureMode();
     entry_t *s = NULL;
     for (int i = 0; i < num_nodes; i++)
     {
+        printf("%d/%d\n", i, num_nodes);
         for (int j = i + 1; j < num_nodes; j++)
         {
             entry_t *m = (entry_t *)malloc(sizeof(entry_t));
@@ -67,7 +63,7 @@ void main(int argc, char **argv)
             m->b = &nodes[j];
             int32_t x = (int32_t)nodes[i].x - (int32_t)nodes[j].x;
             int32_t y = (int32_t)nodes[i].y - (int32_t)nodes[j].y;
-            m->d = sqrt(x * x + y * y);
+            m->d = (x * x + y * y);
             m->next = NULL;
             if (s == NULL)
             {
@@ -94,10 +90,32 @@ void main(int argc, char **argv)
             }
         }
     }
+    InitWindow(WIDTH, HEIGHT, "Kruskal's algorithm");
+    //ToggleFullscreen();
+    target = LoadRenderTexture(WIDTH, HEIGHT);
+    target_2 = LoadRenderTexture(WIDTH, HEIGHT);
+    BeginTextureMode(target);
+    ClearBackground((Color){0, 0, 0, 0});
+    for (int i = 0; i < num_nodes; i++)
+    {
+        DrawCircle(nodes[i].x, nodes[i].y, 4, RAYWHITE);
+    }
+    EndTextureMode();
+    BeginTextureMode(target_2);
+    {
+        ClearBackground((Color){0, 0, 0, 0});
+        entry_t *tmp = s;
+        while (tmp != NULL)
+        {
+            DrawLineEx((Vector2){tmp->a->x, tmp->a->y}, (Vector2){tmp->b->x, tmp->b->y}, 1, (Color){245, 245, 245, 125});
+            tmp = tmp->next;
+        }
+    }
+    EndTextureMode();
     entry_t *st = s;
     while (!WindowShouldClose())
     {
-        if (s == NULL)
+        if (s == NULL || num_edges_in_tree == num_nodes - 1)
         {
             BeginDrawing();
             ClearBackground(BLACK);
@@ -117,12 +135,19 @@ void main(int argc, char **argv)
                 s->b->s = p;
                 DrawLineEx((Vector2){s->a->x, s->a->y}, (Vector2){s->b->x, s->b->y}, 2, BLUE);
                 EndTextureMode();
+                num_edges_in_tree++;
             }
             BeginDrawing();
             ClearBackground(BLACK);
+            DrawTexturePro(target_2.texture, (Rectangle){0, 0, (float)target_2.texture.width, (float)-target_2.texture.height}, (Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()}, (Vector2){0, 0}, 0.0, WHITE);
             DrawTexturePro(target.texture, (Rectangle){0, 0, (float)target.texture.width, (float)-target.texture.height}, (Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()}, (Vector2){0, 0}, 0.0, WHITE);
             DrawLineEx((Vector2){s->a->x, s->a->y}, (Vector2){s->b->x, s->b->y}, 2, RED);
             //DrawCircle(s->a->x, s->a->y, s->d, (Color){245, 245, 245, 50});
+            {
+                char buffer[8] = {'\0'};
+                sprintf((char*)&buffer, "%.2f%%", ((float)num_edges_in_tree / (float)num_nodes) * 100.0);
+                DrawText(buffer, 10, 10, 20, RAYWHITE);
+            }
             EndDrawing();
             WaitTime(0.25);
             LOG("%d\n", s->d);
